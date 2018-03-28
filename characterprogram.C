9d01ec821a034582a62e76f1afd848aedb8dff95
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-28 16:55:36 +0530

    prgm

---
+++ characterprogram.C
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <conio.h>
+void main()
+{
+  char ch ;
+  clrscr() ;
+  printf("ASCII chart for characters : \n\n") ;
+  for(ch = 65 ; ch <= 122 ; ch++)
+  {
+    if(ch > 90 && ch < 97)
+      continue ;
+int(i=0;i++;i<7);
+{
+    printf("%c \t %3d \t", ch, ch) ;
+    scanf(%d%d%d\n");
+  }
+  getch() ;
+}
